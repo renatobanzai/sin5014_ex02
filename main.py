48fd6854e8e7e204bd5226258a0de6e8c23fed3b
Renato Banzai <renatobanzai@gmail.com>
2021-04-22 01:39:21 -0300

    1st commit

--- main.py
+++ main.py
@@ -55,9 +55,11 @@ class Image:
         # Run each pixel of the image matrix
         for line in range(lines):
             for column in range(columns):
+                # avoid to go less than the min of scale (0)
                 if image_matrix[line, column] + change_value < 0:
                     image_matrix[line, column] = 0
                 else:
+                    # avoid to go upper to max of scale (255)
                     if image_matrix[line, column] + change_value > 255:
                         image_matrix[line, column] = 255
                     else:
@@ -67,17 +69,65 @@ class Image:
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
+    '''
+    Exercício 3
+    Continuar a implementação do programa iniciado no exercício anterior, incluindo
+    um desses filtros (você escolhe):
+        • média
+        • mediana
+        • equalização (x)
+    '''
+    def equalization_filter(self, image_matrix):
+        lines = len(image_matrix)
+        columns = len(image_matrix[0])
+        max_contrast_resolution = 256
+        # creates a vector with a position for each frequency of gray level
+        frequency_gray_levels = [0 for x in range(max_contrast_resolution)]
+        # Run each pixel of the image matrix to get each frequency
+        for line in range(lines):
+            for column in range(columns):
+                pixel_gray_level = image_matrix[line, column]
+                frequency_gray_levels[pixel_gray_level] += 1
 
+        cumulative_value = 0
+        # creates a vector with a position for each cumulative frequency of a gray level
+        cumulative_frequency_gray_levels = [0 for x in range(max_contrast_resolution)]
+        gray_level = 0
+        # run each frequency to determine the cumulative frequency
+        for gray_level in range(max_contrast_resolution):
+            cumulative_value += frequency_gray_levels[gray_level]
+            cumulative_frequency_gray_levels[gray_level] = cumulative_value
 
+        # in class slide, the I
+        ideal_pixels_number = lines * columns / max_contrast_resolution
+
+        # create a dictionary for each gray level with the equalization value
+        equalization_values = {}
+
+        for gray_level in range(max_contrast_resolution):
+            cumulative_frequency = cumulative_frequency_gray_levels[gray_level]
+            equalization_value = max(0, (round((cumulative_frequency/ideal_pixels_number))-1))
+            equalization_values[gray_level] = equalization_value
+
+        # apply the equalization to each images pixel
+        for line in range(lines):
+            for column in range(columns):
+                image_matrix[line, column] = equalization_values[image_matrix[line, column]]
+
+        self.show_histogram(image_matrix)
+        cv2.imshow("imagem", image_matrix)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     img = Image("bird.png")
+
     # Running Exercise 1
-    img.show_histogram(img.image_matrix)
+    #img.show_histogram(img.image_matrix)
 
     # Running Exercise 2
-    img.change_bright(img.image_matrix, -50)
+    #img.change_bright(img.image_matrix, -50)
 
-
-
+    # Running Exercise 3
+    img.equalization_filter(img.image_matrix)
 
